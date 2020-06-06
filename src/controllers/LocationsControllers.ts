b03f4b934b1fb3d510c655671aa0fea8853fd09b
Leonardo Franco <leofranco1305@gmail.com>
2020-06-06 13:36:28 -0300

    Added index, create and show endpoints

--- src/controllers/LocationsControllers.ts
+++ src/controllers/LocationsControllers.ts
@@ -15,18 +15,33 @@ class LocationsController {
         // CHANGE ALL OCURRENCES OF knex TO trx
         const insertedIds = await trx('locations').insert(location)
     
-        const point_id = insertedIds[0]
+        const location_id = insertedIds[0]
     
         const locationItems = items.map( (item_id: number) => {
             return{
                 item_id,
-                point_id
+                location_id
             }
         })
     
         await trx('location_items').insert(locationItems)
         trx.commit()
-        return res.json({ id:point_id, ...location })
+        return res.json({ id:location_id, ...location })
+    }
+
+    async show (req: Request,res:Response) {
+        const { id } = req.params
+        
+        const location = await knex('locations').where('id', id).first()
+
+        if (!location) return res.status(400).json({messgae: 'Location not found'})
+        
+        const items = await knex('items')
+            .join('location_items','items.id','=','location_items.item_id')
+            .where('location_items.location_id', id )
+            .select('items.title')
+        
+        return res.json({ location, items })
     }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,13 @@
 import express from 'express'
-import knex from './db/connection'
 import LocationsControllers from './controllers/LocationsControllers'
-// import itemsControllers from './controllers/itemsControllers'
+import ItemsControllers from './controllers/ItemsControllers'
 
 const router = express.Router()
 const locationsControllers = new LocationsControllers()
+const itemsControllers = new ItemsControllers()
 
-router.get('/items', async (req, res) => {
-    const items = await knex('items').select('*')
-    const serializedItems = items.map( item => {
-        return{
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        }
-    })
-    return res.json({ serializedItems })
-})
-
+router.get('/items', itemsControllers.index)
 router.post('/locations', locationsControllers.create)
+router.get('/locations/:id', locationsControllers.show)
 
 export default router
